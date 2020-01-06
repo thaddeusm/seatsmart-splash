f36b8c86c416ff6ac6b26e1143ecdd1b288a523a
Thaddeus McCleary <thaddeusmccleary@yahoo.com>
2020-01-06 16:31:18 -0600

    Update year

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <h6>Released under the MIT License | <img src="@/assets/github.svg" alt="github logo" id="github"><a href="https://github.com/thaddeusm/seatsmart-FHSU" target="_blank">Source Code</a></h6>
-        <h6>Copyright © 2019 <a href="mailto:tbmccleary@fhsu.edu">Thaddeus McCleary</a></h6>
+        <h6>Copyright © 2020 <a href="mailto:tbmccleary@fhsu.edu">Thaddeus McCleary</a></h6>
         <h6><a href="#" v-scroll-to="'#navigationContainer'"><img src="@/assets/up.svg" alt="up arrow" id="up"></a></h6>
     </footer>
 </template>
